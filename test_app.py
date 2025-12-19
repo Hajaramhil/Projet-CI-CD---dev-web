435ee023c2448160dfb35878b0313b695155c08d
katiaaa <katia.aitbachir@gmail.com>
2025-12-19 12:02:32 +0100

    partie docker

--- test_app.py
+++ test_app.py
@@ -6,3 +6,7 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 404
+
+def test_home_content():
+    response = client.get("/")
+    assert response.json()["message"] == "CI/CD project works"
