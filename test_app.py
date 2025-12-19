75588164dce9a825fb292596315749f5ab068444
katiaaa <katia.aitbachir@gmail.com>
2025-12-19 12:11:46 +0100

    test docker

--- test_app.py
+++ test_app.py
@@ -1,8 +1,15 @@
 from fastapi.testclient import TestClient
 from main import app
 
+
 client = TestClient(app)
+
 
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_home_content():
+    response = client.get("/")
+    assert response.json()["message"] == "CI/CD project works"
